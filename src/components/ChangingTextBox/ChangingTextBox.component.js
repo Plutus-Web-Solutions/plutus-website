facb9c9b1795c0ee93c40d706e522e402ef74be0
japnitahuja <japnit.ahuja@gmail.com>
2021-08-29 17:31:01 +0530

    Homepage changes

--- src/components/ChangingTextBox/ChangingTextBox.component.js
+++ src/components/ChangingTextBox/ChangingTextBox.component.js
@@ -1,57 +1,49 @@
 import React from "react";
-import {
-  Title,
-  HighlightedWord
-} from "./ChangingTextBox.styles";
+import { Title, HighlightedWord } from "./ChangingTextBox.styles";
 import icon from "../../assets/icon.png";
 import { Container, Row, Col, Image } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.css";
 import NavBar from "../NavBar/NavBar.component";
 
-class ChangingTextBox extends React.Component{
-  constructor(props){
-    super(props)
-    this.state= {
-      words: ["word1", 'word2', 'wordthree'],
-      index: 0
-    }
-
-    
-
+class ChangingTextBox extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      words: ["word1", "word2", "word3"],
+      index: 0,
+    };
   }
 
-  componentDidMount(){
-
-    
+  componentDidMount() {
     setInterval(() => {
       let temp = this.state.index;
-      if(temp===this.state.words.length-1){
-        temp=0
+      if (temp === this.state.words.length - 1) {
+        temp = 0;
+      } else {
+        temp += 1;
       }
-      else{
-        temp+=1
-      }
-      this.setState({index:temp})
-  }, 5000)
+      this.setState({ index: temp });
+    }, 5000);
   }
-   
-  
 
-  render(){
-    let {index, words} = this.state;
-    let {Bgcolor} = this.props;
-   
-    return(
-      <Container fluid className="d-flex justify-content-center" 
-      style={{ backgroundColor: `${Bgcolor}`, height:"30vh" }}>
-      <Row style={{ padding: "2em" }}>
+  render() {
+    let { index, words } = this.state;
+    let { Bgcolor } = this.props;
+
+    return (
+      <Container
+        fluid
+        className="d-flex justify-content-center"
+        style={{ backgroundColor: `${Bgcolor}` }}
+      >
+        <Row style={{ padding: "3em 1em" }}>
           <Title>
             We are <HighlightedWord>{words[index]}</HighlightedWord>
           </Title>
-      </Row>
-    </Container>
+        </Row>
+      </Container>
     );
   }
-};
+}
 
 export default ChangingTextBox;
