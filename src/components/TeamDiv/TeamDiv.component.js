75e872de1fec0833c4faa309cee55053f614d83c
japnitahuja <japnit.ahuja@gmail.com>
2021-11-12 22:55:26 +0530

    changes

--- src/components/TeamDiv/TeamDiv.component.js
+++ src/components/TeamDiv/TeamDiv.component.js
@@ -8,7 +8,6 @@ import {
 import person from "../../assets/person.png";
 import { Container, Row, Col, Image } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.css";
-import krishna from "../../assets/k.png";
 import japnit from "../../assets/j.png";
 import utkarsh from "../../assets/u.png";
 import jaskeerat from "../../assets/jas.png";
@@ -17,17 +16,7 @@ export default function TeamDiv(props) {
   return (
     <Container fluid style={{ marginBottom: "5em" }}>
       <Row>
-        <Col lg={3} style={{ padding: "0" }}>
-          <TeamWrapperDiv>
-            <PictureDiv>
-              <img src={krishna} style={{ height: "100%" }} />
-            </PictureDiv>
-            <NameDiv>Krishnagopal Kedia</NameDiv>
-            <TitleDiv>Head of Technology</TitleDiv>
-          </TeamWrapperDiv>
-        </Col>
-
-        <Col lg={3} style={{ padding: "0" }}>
+        <Col lg={4} style={{ padding: "0" }}>
           <TeamWrapperDiv>
             <PictureDiv>
               <img src={japnit} style={{ height: "100%" }} />
@@ -37,7 +26,7 @@ export default function TeamDiv(props) {
           </TeamWrapperDiv>
         </Col>
 
-        <Col lg={3} style={{ padding: "0" }}>
+        <Col lg={4} style={{ padding: "0" }}>
           <TeamWrapperDiv>
             <PictureDiv>
               <img src={jaskeerat} style={{ height: "100%" }} />
@@ -47,7 +36,7 @@ export default function TeamDiv(props) {
           </TeamWrapperDiv>
         </Col>
 
-        <Col lg={3} style={{ padding: "0" }}>
+        <Col lg={4} style={{ padding: "0" }}>
           <TeamWrapperDiv>
             <PictureDiv>
               <img src={utkarsh} style={{ height: "100%" }} />
